c70643644f1cea5fddf814759d70c81b9ffe2fad
demian.juarez <demian.juarez@gm2dev.com>
2023-10-09 10:06:00 -0300

    Finished

--- src/components/SoloProduct.tsx
+++ src/components/SoloProduct.tsx
@@ -2,27 +2,60 @@ import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../App";
 import { ProductContext } from "../context/ProductContext";
+import { useState } from "react";
+import "./SoloProduct.css";
 
 export const SoloProduct = () => {
   const { id } = useParams();
   const number: number = Number(id);
   const { products } = useContext(ProductContext);
-  const producto = products[number];
+  const producto = products[number - 1];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const carrito = (images: Product["images"]) => {
-    const lengthNumber = images.length;
-    return images[lengthNumber];
+  const carrito = (
+    images: Product["images"],
+    currentImageIndex: number,
+    setCurrentImageIndex: React.Dispatch<React.SetStateAction<number>>
+  ) => {
+    const nextImageIndex = (currentImageIndex + 1) % images.length;
+    setCurrentImageIndex(nextImageIndex);
+    console.log(currentImageIndex);
+    console.log(producto.images);
   };
 
   return (
-    <div className="containerSoloProduct">
-      {producto && (
-        <div className="product" onClick={carrito}>
-          <h2>{producto.title}</h2>
-          <h3>{producto.id}</h3>
-          <img src={`${producto.images[0]}`} />
-        </div>
-      )}
+    <div className="cont">
+      <div className="containerSoloProduct">
+        {producto && (
+          <div className="productoss">
+            <div className="leftside">
+              <h2>{producto.title}</h2>
+              <h3>{producto.id}</h3>
+              <img
+                className="imageProduct"
+                src={`${producto.images[currentImageIndex]}`}
+                onClick={() =>
+                  carrito(
+                    producto.images,
+                    currentImageIndex,
+                    setCurrentImageIndex
+                  )
+                }
+              />
+            </div>
+            <div className="rightside">
+              <div className="content">
+                <p>Marca: {producto.brand}</p>
+                <p>Categoria: {producto.category}</p>
+                <p>Descripcion: {producto.description}</p>
+                <p>Precio: {producto.price}</p>
+                <p>Rating: {producto.rating}/5</p>
+                <p>Stock: {producto.stock}</p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
